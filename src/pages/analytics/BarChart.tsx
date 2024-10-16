dcc3700d8dd7e5f5b3255adcfaad378fba7adfcb
jiayixu420 <jiayixu420@gmail.com>
2024-10-16 05:17:16 -0300

    fixed the bar chart in analytics page

--- src/pages/analytics/BarChart.tsx
+++ src/pages/analytics/BarChart.tsx
@@ -1,21 +1,44 @@
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart } from "@mui/x-charts/BarChart";
+import { MAX } from "uuid";
 
 export interface ChartData {
   data: number[];
   label: string[];
 }
 
-export const BarChartComponent: React.FC<{ datalist: ChartData }> = ({ datalist }) => {  // Destructure datalist here
+export const BarChartComponent: React.FC<{ datalist: ChartData }> = ({
+  datalist,
+}) => {
+  // Destructure datalist here
+  const maxValue = Math.max(...datalist.data);
+  console.log(maxValue);
   return (
-    <BarChart
-      xAxis={[{ scaleType: 'band', data: datalist.label }]}  // Access datalist.label correctly
-      series={[{ data: datalist.data }]}  // Access datalist.data correctly
-      sx={{
-        '& .MuiBarElement-root': { // Target the bar elements specifically
-          border:'1px solid #fff',
-          borderRadius: '16px', // Set the desired border radius for each bar
-        },
-      }}
-    />
+    // <BarChart
+    //   xAxis={[{ scaleType: 'band', data: datalist.label }]}  // Access datalist.label correctly
+    //   series={[{ data: datalist.data }]}  // Access datalist.data correctly
+    //   sx={{
+    //     '& .MuiBarElement-root': { // Target the bar elements specifically
+    //       border:'1px solid #fff',
+    //       borderRadius: '16px', // Set the desired border radius for each bar
+    //     },
+    //   }}
+    // />
+    <div className="flex flex-row justify-evenly items-end gap-x-[10px] h-[100px]">
+      {datalist &&
+        datalist.label.map((item, index) => (
+          <div className="flex flex-col gap-y-2 items-center" key={index}>
+            <div
+              className="w-[12px]"
+              style={{
+                height: `${67 * datalist.data[index] / maxValue}px`,
+                backgroundColor: "#4152EC",
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20
+              }}
+            ></div>
+            <p className="b5">{datalist.label[index]}</p>
+          </div>
+        ))}
+    </div>
   );
 };
